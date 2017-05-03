c117f5e68f2abfd7aa752414e8262fbd257dfb0f
catatsuy <catatsuy@catatsuy.org>
2017-05-03 18:37:02 +0900

    add movabletype

---
+++ parse.go
@@ -0,0 +1,171 @@
+package movabletype
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+// Default
+const (
+	// If it is not inialized, AllowComments is -1
+	DefaultAllowComments = -1
+
+	// If it is not inialized, AllowPings is -1
+	DefaultAllowPings = -1
+)
+
+// Movable Type Import Format
+type MT struct {
+	Author   string
+	Title    string
+	Basename string
+	Status   string
+
+	// 0 or 1. If it is not inialized DefaultAllowComments.
+	AllowComments int
+
+	// 0 or 1. If it is not inialized DefaultAllowPings
+	AllowPings int
+
+	ConvertBreaks string
+
+	Date time.Time
+
+	PrimaryCategory string
+
+	Category []string
+
+	Body string
+
+	ExtendedBody string
+}
+
+// NewMT creates MT.
+func NewMT() *MT {
+	return &MT{
+		AllowComments: DefaultAllowComments,
+		AllowPings:    DefaultAllowPings,
+	}
+}
+
+// Parse creates MT struct from io.Reader
+func Parse(r io.Reader) ([]*MT, error) {
+	mts := []*MT{}
+
+	scanner := bufio.NewScanner(r)
+
+	var err error
+
+	m := NewMT()
+
+	for scanner.Scan() {
+		ss := strings.Split(scanner.Text(), ": ")
+
+		if len(ss) <= 1 {
+			value := ss[0]
+
+			if value == "--------" {
+				mts = append(mts, m)
+				m = NewMT()
+				continue
+			}
+
+			if value == "-----" {
+				continue
+			}
+
+			switch value {
+			case "BODY:":
+				for scanner.Scan() {
+					line := scanner.Text()
+
+					if line == "-----" {
+						break
+					}
+
+					m.Body += line + "\n"
+				}
+				break
+			case "EXTENDED BODY:":
+				for scanner.Scan() {
+					line := scanner.Text()
+
+					if line == "-----" {
+						break
+					}
+
+					m.ExtendedBody += line + "\n"
+				}
+				break
+			}
+
+			continue
+		}
+
+		key, value := ss[0], ss[1]
+
+		switch key {
+		case "AUTHOR":
+			m.Author = value
+			break
+		case "TITLE":
+			m.Title = value
+			break
+		case "BASENAME":
+			m.Basename = value
+			break
+		case "STATUS":
+			if value == "Draft" || value == "Publish" || value == "Future" {
+				m.Status = value
+			} else {
+				return nil, fmt.Errorf("STATUS column is allowed only Draft or Publish or Future. Got %s", value)
+			}
+			break
+		case "ALLOW COMMENTS":
+			m.AllowComments, err = strconv.Atoi(value)
+			if err != nil {
+				return nil, errors.Wrap(err, "ALLOW COMMENTS column is allowed only 0 or 1")
+			}
+			if m.AllowComments != 0 && m.AllowComments != 1 {
+				return nil, fmt.Errorf("ALLOW COMMENTS column is allowed only 0 or 1. Got %d", m.AllowComments)
+			}
+			break
+		case "ALLOW PINGS":
+			m.AllowPings, err = strconv.Atoi(value)
+			if err != nil {
+				return nil, errors.Wrap(err, "ALLOW PINGS column is allowed only 0 or 1")
+			}
+			if m.AllowComments != 0 && m.AllowComments != 1 {
+				return nil, fmt.Errorf("ALLOW PINGS column is allowed only 0 or 1. Got %d", m.AllowPings)
+			}
+			break
+		case "CONVERT BREAKS":
+			m.ConvertBreaks = value
+			break
+		case "DATE":
+			if strings.HasSuffix(value, "AM") || strings.HasSuffix(value, "PM") {
+				m.Date, err = time.Parse("01/02/2006 03:04:05 PM", value)
+			} else {
+				m.Date, err = time.Parse("01/02/2006 15:04:05", value)
+			}
+			if err != nil {
+				return nil, errors.Wrap(err, "Parsing error on DATE column")
+			}
+			break
+		case "PRIMARY CATEGORY":
+			m.PrimaryCategory = value
+			break
+		case "CATEGORY":
+			m.Category = append(m.Category, value)
+			break
+		}
+	}
+
+	return mts, nil
+}
---
+++ parse_test.go
@@ -0,0 +1,148 @@
+package movabletype_test
+
+import (
+	"bytes"
+	"io"
+	"reflect"
+	"testing"
+	"time"
+
+	. "github.com/catatsuy/movabletype"
+)
+
+func TestParse(t *testing.T) {
+	buf := bytes.NewBufferString(`AUTHOR: catatsuy
+TITLE: ポエム
+BASENAME: poem
+STATUS: Publish
+ALLOW COMMENTS: 1
+ALLOW PINGS: 1
+CONVERT BREAKS: 0
+DATE: 04/22/2017 20:41:58
+PRIMARY CATEGORY: ブログ
+CATEGORY: ポエム
+CATEGORY: 技術系
+-----
+BODY:
+<p>body</p>
+<p>bodybody</p>
+<p>bodybodybody</p>
+-----
+EXTENDED BODY:
+<p>extended body</p>
+<p>extended body body</p>
+<p>extended body body body</p>
+-----
+--------
+AUTHOR: catatsuy
+TITLE: 風邪で声を失った話
+BASENAME: 2017/04/09/194939
+STATUS: Publish
+ALLOW COMMENTS: 1
+CONVERT BREAKS: 0
+DATE: 04/09/2017 07:49:39 PM
+CATEGORY: 日常
+-----
+BODY:
+<p>bodybodybody</p>
+-----
+EXTENDED BODY:
+<p>extended body body body</p>
+-----
+--------
+`)
+
+	expected := []*MT{
+		&MT{
+			Author:          "catatsuy",
+			Title:           "ポエム",
+			Basename:        "poem",
+			Status:          "Publish",
+			AllowComments:   1,
+			AllowPings:      1,
+			ConvertBreaks:   "0",
+			Date:            time.Date(2017, time.April, 22, 20, 41, 58, 0, time.UTC),
+			PrimaryCategory: "ブログ",
+			Category:        []string{"ポエム", "技術系"},
+			Body:            "<p>body</p>\n<p>bodybody</p>\n<p>bodybodybody</p>\n",
+			ExtendedBody:    "<p>extended body</p>\n<p>extended body body</p>\n<p>extended body body body</p>\n",
+		},
+		&MT{
+			Author:        "catatsuy",
+			Title:         "風邪で声を失った話",
+			Basename:      "2017/04/09/194939",
+			Status:        "Publish",
+			AllowComments: 1,
+			AllowPings:    -1,
+			ConvertBreaks: "0",
+			Date:          time.Date(2017, time.April, 9, 19, 49, 39, 0, time.UTC),
+			Category:      []string{"日常"},
+			Body:          "<p>bodybodybody</p>\n",
+			ExtendedBody:  "<p>extended body body body</p>\n",
+		},
+	}
+
+	mts, err := Parse(buf)
+
+	if err != nil {
+		t.Fatalf("got error %q", err)
+	}
+
+	if !reflect.DeepEqual(mts, expected) {
+		t.Errorf("Error parsing, expected %q; got %q", expected, mts)
+	}
+}
+
+func TestParseStatusNotAllowed(t *testing.T) {
+	buf := bytes.NewBufferString(`STATUS: Published`)
+
+	_, err := Parse(buf)
+
+	if err == nil || err.Error() != "STATUS column is allowed only Draft or Publish or Future. Got Published" {
+		t.Errorf("Error parsing, got %q", err)
+	}
+}
+
+func TestParseDate(t *testing.T) {
+	var featuretests = []struct {
+		buf io.Reader
+		t   time.Time
+	}{
+		{
+			bytes.NewBufferString("DATE: 04/22/2017 08:41:58 PM\n--------\n"),
+			time.Date(2017, time.April, 22, 20, 41, 58, 0, time.UTC),
+		},
+		{
+			bytes.NewBufferString("DATE: 04/22/2017 08:41:58 AM\n--------\n"),
+			time.Date(2017, time.April, 22, 8, 41, 58, 0, time.UTC),
+		},
+		{
+			bytes.NewBufferString("DATE: 04/22/2017 20:41:58\n--------\n"),
+			time.Date(2017, time.April, 22, 20, 41, 58, 0, time.UTC),
+		},
+	}
+
+	for _, ft := range featuretests {
+		mts, err := Parse(ft.buf)
+
+		if err != nil {
+			t.Fatalf("got error %q", err)
+		}
+
+		if mts[0].Date != ft.t {
+			t.Errorf("m.Date got %v; want %v", mts[0].Date, ft.t)
+		}
+	}
+}
+
+func TestNewMT(t *testing.T) {
+	m := NewMT()
+
+	if m.AllowComments != DefaultAllowComments {
+		t.Errorf("By default, AllowComments is %d, got %d", DefaultAllowComments, m.AllowComments)
+	}
+
+	if m.AllowPings != DefaultAllowPings {
+		t.Errorf("By default, AllowComments is %d, got %d", DefaultAllowPings, m.AllowPings)
+	}
+}
